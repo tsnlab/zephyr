df3afb6f5f84e57d926625f67a66194796383ec9
pooky1004 <hounjoung@tsnlab.com>
2024-10-01 10:12:51 +0900

    보드 초기화 기능 개발/Power Management Unit 초기화기능 개발

--- soc/tcc/tcc7045/soc.h
+++ soc/tcc/tcc7045/soc.h
@@ -312,4 +312,184 @@ enum {
 
 #define GIC_INT_SRC_CNT (GIC_SPU + 1UL)
 
+/* pmu start */
+
+#define PMU_VA_WR_PASS (0x5AFEACE5UL)
+
+typedef enum {
+	PMU_FMU_NOTHING = 0,
+
+	PMU_FMU_XIN_PVT_DISABLE = 1,
+	PMU_FMU_XIN_PVT_EDGE = 2,
+	PMU_FMU_XIN_PVT_LEVEL = 3,
+
+	PMU_FMU_PWR_PVT_DISABLE = 4,
+	PMU_FMU_PWR_PVT_EDGE = 5,
+	PMU_FMU_PWR_PVT_LEVEL = 6,
+
+	PMU_FMU_MAX = 7
+} PMUFmu_t;
+
+#define PMU_VA_MASK_1  (0x1UL)
+#define PMU_VA_MASK_2  (0x3UL)
+#define PMU_VA_MASK_3  (0x7UL)
+#define PMU_VA_MASK_4  (0xFUL)
+#define PMU_VA_MASK_5  (0x1FUL)
+#define PMU_VA_MASK_6  (0x3FUL)
+#define PMU_VA_MASK_7  (0x7FUL)
+#define PMU_VA_MASK_8  (0xFFUL)
+#define PMU_VA_MASK_9  (0x1FFUL)
+#define PMU_VA_MASK_10 (0x3FFUL)
+#define PMU_VA_MASK_11 (0x7FFUL)
+#define PMU_VA_MASK_12 (0xFFFUL)
+#define PMU_VA_MASK_13 (0x1FFFUL)
+#define PMU_VA_MASK_14 (0x3FFFUL)
+#define PMU_VA_MASK_15 (0x7FFFUL)
+#define PMU_VA_MASK_16 (0xFFFFUL)
+#define PMU_VA_MASK_17 (0x1FFFFUL)
+#define PMU_VA_MASK_18 (0x3FFFFUL)
+#define PMU_VA_MASK_19 (0x7FFFFUL)
+#define PMU_VA_MASK_20 (0xFFFFFUL)
+#define PMU_VA_MASK_21 (0x1FFFFFUL)
+#define PMU_VA_MASK_22 (0x3FFFFFUL)
+#define PMU_VA_MASK_23 (0x7FFFFFUL)
+#define PMU_VA_MASK_24 (0xFFFFFFUL)
+#define PMU_VA_MASK_25 (0x1FFFFFFUL)
+#define PMU_VA_MASK_26 (0x3FFFFFFUL)
+#define PMU_VA_MASK_27 (0x7FFFFFFUL)
+#define PMU_VA_MASK_28 (0xFFFFFFFUL)
+#define PMU_VA_MASK_29 (0x1FFFFFFFUL)
+#define PMU_VA_MASK_30 (0x3FFFFFFFUL)
+#define PMU_VA_MASK_31 (0x7FFFFFFFUL)
+#define PMU_VA_MASK_32 (0xFFFFFFFFUL)
+
+#define PMU_ADDR_BASE              (0xA0F28000)
+#define PMU_ADDR_GLB_CONFIG        (PMU_ADDR_BASE + 0x00UL)
+#define PMU_ADDR_RST_STATUS0       (PMU_ADDR_BASE + 0x04UL)
+#define PMU_ADDR_RST_STATUS1       (PMU_ADDR_BASE + 0x08UL)
+#define PMU_ADDR_RST_ENABLE        (PMU_ADDR_BASE + 0x0CUL)
+#define PMU_ADDR_COLD_RST_REQ      (PMU_ADDR_BASE + 0x10UL)
+#define PMU_ADDR_WARM_RST_REQ      (PMU_ADDR_BASE + 0x14UL)
+#define PMU_ADDR_HSM_RSTN_MSK      (PMU_ADDR_BASE + 0x18UL)
+#define PMU_ADDR_HSM_SWRSTN        (PMU_ADDR_BASE + 0x1CUL)
+#define PMU_ADDR_HSM_STATUS        (PMU_ADDR_BASE + 0x20UL)
+#define PMU_ADDR_SYSRST_CTRL       (PMU_ADDR_BASE + 0x24UL)
+#define PMU_ADDR_MEM_ECC_CFG       (PMU_ADDR_BASE + 0x28UL)
+#define PMU_ADDR_PVT0_CFG          (PMU_ADDR_BASE + 0x2CUL)
+#define PMU_ADDR_PVT1_CFG          (PMU_ADDR_BASE + 0x30UL)
+#define PMU_ADDR_PVT2_CFG          (PMU_ADDR_BASE + 0x34UL)
+#define PMU_ADDR_PVT3_CFG          (PMU_ADDR_BASE + 0x38UL)
+#define PMU_ADDR_PVT4_CFG          (PMU_ADDR_BASE + 0x3CUL)
+#define PMU_ADDR_PVT_SM_CFG        (PMU_ADDR_BASE + 0x40UL)
+#define PMU_ADDR_XIN_SELECT_FREQ   (PMU_ADDR_BASE + 0x44UL)
+#define PMU_ADDR_OSC_CFG           (PMU_ADDR_BASE + 0x48UL)
+#define PMU_ADDR_ECID_USER0_FBOUT0 (PMU_ADDR_BASE + 0x4CUL)
+#define PMU_ADDR_ECID_USER0_FBOUT1 (PMU_ADDR_BASE + 0x50UL)
+#define PMU_ADDR_ECID_USER0_FBOUT2 (PMU_ADDR_BASE + 0x54UL)
+#define PMU_ADDR_ECID_USER1_FBOUT0 (PMU_ADDR_BASE + 0x58UL)
+#define PMU_ADDR_ECID_USER1_FBOUT1 (PMU_ADDR_BASE + 0x5CUL)
+#define PMU_ADDR_ECID_USER1_FBOUT2 (PMU_ADDR_BASE + 0x60UL)
+#define PMU_ADDR_MEM_CFG           (PMU_ADDR_BASE + 0x64UL)
+#define PMU_ADDR_BACKUP_REG        (PMU_ADDR_BASE + 0x68UL)
+#define PMU_ADDR_PMU_WDT_EN        (PMU_ADDR_BASE + 0x6CUL)
+#define PMU_ADDR_PMU_WDT_CLR       (PMU_ADDR_BASE + 0x70UL)
+#define PMU_ADDR_PMU_WDT_IRQ_CNT   (PMU_ADDR_BASE + 0x74UL)
+#define PMU_ADDR_PMU_WDT_RST_CNT   (PMU_ADDR_BASE + 0x78UL)
+#define PMU_ADDR_PMU_WDT_SM_MODE   (PMU_ADDR_BASE + 0x7CUL)
+#define PMU_ADDR_PMU_WDT_LOCK      (PMU_ADDR_BASE + 0x80UL)
+#define PMU_ADDR_PMU_WR_PW         (PMU_ADDR_BASE + 0x3FCUL)
+
+#define PMU_ADDR_COMMON_FIELD_ZERO       (0UL)
+#define PMU_ADDR_COMMON_FIELD_FULL_MASK  (PMU_VA_MASK_31)
+#define PMU_ADDR_COMMON_FIELD_CLEAR_MASK (0UL)
+
+/* FIELD INFO (Register: PVT_SM_CFG) ===========================================*/
+#define PMU_ADDR_PVT_SM_CFG_FIELD_POWER_SM_FMU_EN (3UL)
+#define PMU_ADDR_PVT_SM_CFG_FIELD_POWER_SM_FMU_EN_MASK                                             \
+	(PMU_VA_MASK_1 << PMU_ADDR_PVT_SM_CFG_FIELD_POWER_SM_FMU_EN)
+#define PMU_ADDR_PVT_SM_CFG_FIELD_POWER_PERIODIC_EN (2UL)
+#define PMU_ADDR_PVT_SM_CFG_FIELD_POWER_PERIODIC_EN_MASK                                           \
+	(PMU_VA_MASK_1 << PMU_ADDR_PVT_SM_CFG_FIELD_POWER_PERIODIC_EN)
+#define PMU_ADDR_PVT_SM_CFG_FIELD_XIN_SM_FMU_EN (1UL)
+#define PMU_ADDR_PVT_SM_CFG_FIELD_XIN_SM_FMU_EN_MASK                                               \
+	(PMU_VA_MASK_1 << PMU_ADDR_PVT_SM_CFG_FIELD_XIN_SM_FMU_EN)
+#define PMU_ADDR_PVT_SM_CFG_FIELD_XIN_PERIODIC_EN (0UL)
+#define PMU_ADDR_PVT_SM_CFG_FIELD_XIN_PERIODIC_EN_MASK                                             \
+	(PMU_VA_MASK_1 << PMU_ADDR_PVT_SM_CFG_FIELD_XIN_PERIODIC_EN)
+
+/* FIELD INFO (Register: PVT4_CFG) ===========================================*/
+#define PMU_ADDR_PVT4_CFG_FIELD_XIN_MON_EN (16UL)
+#define PMU_ADDR_PVT4_CFG_FIELD_XIN_MON_EN_MASK                                                    \
+	(PMU_VA_MASK_1 << PMU_ADDR_PVT4_CFG_FIELD_XIN_MON_EN)
+#define PMU_ADDR_PVT4_CFG_FIELD_CFG_XIN_MON_SETTLE (13UL)
+#define PMU_ADDR_PVT4_CFG_FIELD_CFG_XIN_MON_SETTLE_MASK                                            \
+	(PMU_VA_MASK_3 << PMU_ADDR_PVT4_CFG_FIELD_CFG_XIN_MON_SETTLE)
+#define PMU_ADDR_PVT4_CFG_FIELD_XIN_OURANGE (12UL)
+#define PMU_ADDR_PVT4_CFG_FIELD_XIN_OURANGE_MASK                                                   \
+	(PMU_VA_MASK_1 << PMU_ADDR_PVT4_CFG_FIELD_XIN_OURANGE)
+#define PMU_ADDR_PVT4_CFG_FIELD_OSC_XIN_CAPTURE (8UL)
+#define PMU_ADDR_PVT4_CFG_FIELD_OSC_XIN_CAPTURE_MASK                                               \
+	(PMU_VA_MASK_4 << PMU_ADDR_PVT4_CFG_FIELD_OSC_XIN_CAPTURE)
+#define PMU_ADDR_PVT4_CFG_FIELD_OSC_XIN_RANGE (0UL)
+#define PMU_ADDR_PVT4_CFG_FIELD_OSC_XIN_RANGE_MASK                                                 \
+	(PMU_VA_MASK_8 << PMU_ADDR_PVT4_CFG_FIELD_OSC_XIN_RANGE)
+
+/* FIELD INFO (Register: BACKUP_REG) ===========================================*/
+#define PMU_ADDR_BACKUP_REG_FIELD_BACKUP_REG (0UL)
+#define PMU_ADDR_BACKUP_REG_FIELD_BACKUP_REG_MASK                                                  \
+	(PMU_VA_MASK_32 << PMU_ADDR_BACKUP_REG_FIELD_BACKUP_REG)
+
+/* FIELD INFO (Register: PMU_WDT_EN) ===========================================*/
+#define PMU_ADDR_PMU_WDT_EN_FIELD_WDT_EN      (0UL)
+#define PMU_ADDR_PMU_WDT_EN_FIELD_WDT_EN_MASK (PMU_VA_MASK_1 << PMU_ADDR_PMU_WDT_EN_FIELD_WDT_EN)
+
+/* FIELD INFO (Register: PMU_WDT_CLR) ===========================================*/
+#define PMU_ADDR_PMU_WDT_CLR_FIELD_WDT_RST_CLR (1UL)
+#define PMU_ADDR_PMU_WDT_CLR_FIELD_WDT_RST_CLR_MASK                                                \
+	(PMU_VA_MASK_1 << PMU_ADDR_PMU_WDT_CLR_FIELD_WDT_RST_CLR)
+#define PMU_ADDR_PMU_WDT_CLR_FIELD_WDT_CLR (0UL)
+#define PMU_ADDR_PMU_WDT_CLR_FIELD_WDT_CLR_MASK                                                    \
+	(PMU_VA_MASK_1 << PMU_ADDR_PMU_WDT_CLR_FIELD_WDT_CLR)
+
+/* FIELD INFO (Register: PMU_WDT_IRQ_CNT) ===========================================*/
+#define PMU_ADDR_PMU_WDT_IRQ_CNT_FIELD_WDT_IRQ_VALUE (0UL)
+#define PMU_ADDR_PMU_WDT_IRQ_CNT_FIELD_WDT_IRQ_VALUE_MASK                                          \
+	(PMU_VA_MASK_32 << PMU_ADDR_PMU_WDT_IRQ_CNT_FIELD_WDT_IRQ_VALUE)
+
+/* FIELD INFO (Register: PMU_WDT_RST_CNT) ===========================================*/
+#define PMU_ADDR_PMU_WDT_RST_CNT_FIELD_WDT_RST_CNT (0UL)
+#define PMU_ADDR_PMU_WDT_RST_CNT_FIELD_WDT_RST_CNT_MASK                                            \
+	(PMU_VA_MASK_32 << PMU_ADDR_PMU_WDT_RST_CNT_FIELD_WDT_RST_CNT)
+
+/* FIELD INFO (Register: PMU_WDT_SM_MODE) ===========================================*/
+#define PMU_ADDR_PMU_WDT_SM_MODE_FIELD_CONT (3UL)
+#define PMU_ADDR_PMU_WDT_SM_MODE_FIELD_CONT_MASK                                                   \
+	(PMU_VA_MASK_1 << PMU_ADDR_PMU_WDT_SM_MODE_FIELD_CONT)
+#define PMU_ADDR_PMU_WDT_SM_MODE_FIELD_RST_SEL (2UL)
+#define PMU_ADDR_PMU_WDT_SM_MODE_FIELD_RST_SEL_MASK                                                \
+	(PMU_VA_MASK_1 << PMU_ADDR_PMU_WDT_SM_MODE_FIELD_RST_SEL)
+#define PMU_ADDR_PMU_WDT_SM_MODE_FIELD_2oo3 (0UL)
+#define PMU_ADDR_PMU_WDT_SM_MODE_FIELD_2oo3_MASK                                                   \
+	(PMU_VA_MASK_2 << PMU_ADDR_PMU_WDT_SM_MODE_FIELD_2oo3)
+
+/* FIELD INFO (Register: PMU_WDT_LOCK) ===========================================*/
+#define PMU_ADDR_PMU_WDT_LOCK_FIELD_PMU_WDT_LOCK (0UL)
+#define PMU_ADDR_PMU_WDT_LOCK_FIELD_PMU_WDT_LOCK_MASK                                              \
+	(PMU_VA_MASK_32 << PMU_ADDR_PMU_WDT_LOCK_FIELD_PMU_WDT_LOCK)
+
+/* FIELD INFO (Register: PMU_WR_PW) ===========================================*/
+#define PMU_ADDR_PMU_WR_PW_FIELD_PMU_WR_PW (0UL)
+#define PMU_ADDR_PMU_WR_PW_FIELD_PMU_WR_PW_MASK                                                    \
+	(PMU_VA_MASK_32 << PMU_ADDR_PMU_WR_PW_FIELD_PMU_WR_PW)
+
+#define PMU_REG_GET(o, m, a)                                                                       \
+	(((*(volatile unsigned long *)((unsigned long)a)) & (unsigned long)m) >> (unsigned long)o)
+#define PMU_REG_SET(x, o, m, a)                                                                    \
+	(*(volatile unsigned long *)((unsigned long)a)) =                                          \
+		(((unsigned long)x << (unsigned long)o) & (unsigned long)m)
+#define PMU_REG_APPEND(x, o, m, a)                                                                 \
+	((*(volatile unsigned long *)((unsigned long)a)) =                                         \
+		 (((*(volatile unsigned long *)((unsigned long)a)) & (~((unsigned long)m))) |      \
+		  ((unsigned long)x << o)))
+
 #endif /* _BOARD__H_ */
