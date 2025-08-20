ee3cf63d1bf9b7ac2bc0fbe42bda2c46f92f59da
Junho Lee <junho@tsnlab.com>
2025-08-20 19:54:18 +0900

    Add perf examples

--- samples/t1s/src/arp.h
+++ samples/t1s/src/arp.h
@@ -12,5 +12,4 @@
 int send_arp_request(const struct spi_dt_spec* spi, const uint8_t my_mac_addr[ETH_ALEN], const uint8_t my_ip_addr[IP_LEN], const uint8_t target_ip_addr[IP_LEN]);
 int receive_arp_reply(const struct spi_dt_spec* spi);
 
-
 #endif  /* ARP_H */
---
+++ samples/t1s/src/perf.h
@@ -0,0 +1,48 @@
+#ifndef PERF_H
+#define PERF_H
+
+#include <stdint.h>
+#include <zephyr/drivers/spi.h>
+
+#include "eth.h"
+
+struct perf_header {
+    uint32_t id;
+    uint8_t op;
+} __attribute__((packed));
+
+struct perf_startreq_header {
+    struct perf_header header;
+    uint32_t duration; 
+    uint32_t warmup; 
+} __attribute__((packed));
+
+struct perf_latency_header {
+    uint32_t id;
+    uint8_t op;
+    uint32_t tv_sec;
+    uint32_t tv_usec;
+} __attribute__((packed));
+
+enum perf_op {
+    PERF_REQ_START = 0x00,
+    PERF_REQ_END = 0x01,
+    PERF_RES_START = 0x20,
+    PERF_RES_END = 0x21,
+    PERF_DATA = 0x30,
+    PERF_REQ_RESULT = 0x40,
+    PERF_RES_RESULT = 0x41,
+};
+
+#define PERF_ETHERTYPE 0x1337
+#define MAX_PERF_PAYLOAD 1500
+
+int send_perf_req_packet(const struct spi_dt_spec* spi, const uint8_t my_mac_addr[ETH_ALEN], const uint8_t target_mac_addr[ETH_ALEN], uint32_t duration, uint32_t warmup);
+uint16_t make_perf_data_packet(const uint8_t my_mac_addr[ETH_ALEN], const uint8_t target_mac_addr[ETH_ALEN], uint8_t* packet, uint32_t len);
+int send_perf_data_packet(const struct spi_dt_spec* spi, const uint8_t* packet, uint16_t length);
+
+int recv_latency_req(const struct spi_dt_spec* spi, uint8_t source_mac_addr[ETH_ALEN], uint32_t* id);
+uint16_t make_latency_res(uint8_t* packet, const uint8_t my_mac_addr[ETH_ALEN], const uint8_t target_mac_addr[ETH_ALEN], uint32_t id);
+int send_latency_res(const struct spi_dt_spec* spi, const uint8_t* packet, uint16_t length);
+
+#endif /* PERF_H */
