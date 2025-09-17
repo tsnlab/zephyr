fe5a734cfcbdb5cbc8ccc33135920418d580a984
Junho Lee <junho@tsnlab.com>
2025-09-17 13:29:03 +0900

    Apply clang-format

--- drivers/spi/spi_bcm2711.c
+++ drivers/spi/spi_bcm2711.c
@@ -16,17 +16,17 @@ LOG_MODULE_REGISTER(spi_bcm2711);
 
 #include "spi_context.h"
 
-#define SPI_CS(base) (base + 0x00)
+#define SPI_CS(base)   (base + 0x00)
 #define SPI_FIFO(base) (base + 0x04)
-#define SPI_CLK(base) (base + 0x08)
+#define SPI_CLK(base)  (base + 0x08)
 #define SPI_DLEN(base) (base + 0x0C)
 #define SPI_LTOH(base) (base + 0x10)
-#define SPI_DC(base) (base + 0x14)
+#define SPI_DC(base)   (base + 0x14)
 
-#define SPI_CS_CPHA BIT(2)
-#define SPI_CS_CPOL BIT(3)
+#define SPI_CS_CPHA  BIT(2)
+#define SPI_CS_CPOL  BIT(3)
 #define SPI_CS_CSPOL BIT(6)
-#define SPI_CS_TA BIT(7)
+#define SPI_CS_TA    BIT(7)
 
 #define SPI_CS_RXD BIT(17)
 #define SPI_CS_TXD BIT(18)
@@ -39,22 +39,22 @@ LOG_MODULE_REGISTER(spi_bcm2711);
 #define DEV_DATA(dev) ((struct spi_bcm2711_data *const)(dev)->data)
 
 struct spi_bcm2711_config {
-    DEVICE_MMIO_NAMED_ROM(reg_base);
+	DEVICE_MMIO_NAMED_ROM(reg_base);
 
-    struct gpio_dt_spec clk_gpio;
-    struct gpio_dt_spec miso_gpio;
-    struct gpio_dt_spec mosi_gpio;
-    struct gpio_dt_spec cs_gpio;
+	struct gpio_dt_spec clk_gpio;
+	struct gpio_dt_spec miso_gpio;
+	struct gpio_dt_spec mosi_gpio;
+	struct gpio_dt_spec cs_gpio;
 
-    uint32_t clock_freq;
+	uint32_t clock_freq;
 };
 
 struct spi_bcm2711_data {
-    DEVICE_MMIO_NAMED_RAM(reg_base);
-    mem_addr_t base;
+	DEVICE_MMIO_NAMED_RAM(reg_base);
+	mem_addr_t base;
 
 	struct spi_context ctx;
-    uint8_t dfs;
+	uint8_t dfs;
 };
 
 static int spi_bcm2711_configure(const struct device *port, const struct spi_config *spi_cfg)
@@ -67,8 +67,8 @@ static int spi_bcm2711_configure(const struct device *port, const struct spi_con
 		return 0;
 	}
 
-    uint32_t clk = config->clock_freq / spi_cfg->frequency;
-    sys_write32(clk, SPI_CLK(data->base));
+	uint32_t clk = config->clock_freq / spi_cfg->frequency;
+	sys_write32(clk, SPI_CLK(data->base));
 
 	if (spi_cfg->operation & SPI_OP_MODE_SLAVE) {
 		LOG_ERR("Slave mode is not supported");
@@ -139,19 +139,22 @@ static int spi_bcm2711_xfer(const struct device *port)
 	sys_write32(cs, SPI_CS(data->base));
 
 	cs = sys_read32(SPI_CS(data->base));
-	while (spi_context_tx_buf_on(&data->ctx) || (spi_context_rx_buf_on(&data->ctx) && (cs & SPI_CS_RXD))) {
+	while (spi_context_tx_buf_on(&data->ctx) ||
+	       (spi_context_rx_buf_on(&data->ctx) && (cs & SPI_CS_RXD))) {
 		/* Tx */
 		if (spi_context_tx_buf_on(&data->ctx) && (cs & SPI_CS_TXD)) {
 			switch (data->dfs) {
-				case DFS_4B:
-					sys_write32(sys_get_be32(data->ctx.tx_buf), SPI_FIFO(data->base));
-					break;
-				case DFS_2B:
-					sys_write32((uint32_t)sys_get_be16(data->ctx.tx_buf), SPI_FIFO(data->base));
-					break;
-				case DFS_1B:
-					sys_write32((uint32_t)*(uint8_t *)data->ctx.tx_buf, SPI_FIFO(data->base));
-					break;
+			case DFS_4B:
+				sys_write32(sys_get_be32(data->ctx.tx_buf), SPI_FIFO(data->base));
+				break;
+			case DFS_2B:
+				sys_write32((uint32_t)sys_get_be16(data->ctx.tx_buf),
+					    SPI_FIFO(data->base));
+				break;
+			case DFS_1B:
+				sys_write32((uint32_t)*(uint8_t *)data->ctx.tx_buf,
+					    SPI_FIFO(data->base));
+				break;
 			}
 			spi_context_update_tx(&data->ctx, data->dfs, 1);
 		}
@@ -159,15 +162,15 @@ static int spi_bcm2711_xfer(const struct device *port)
 		/* Rx */
 		if (spi_context_rx_buf_on(&data->ctx) && (cs & SPI_CS_RXD)) {
 			switch (data->dfs) {
-				case DFS_4B:
-					sys_put_be32(sys_read32(SPI_FIFO(data->base)), data->ctx.rx_buf);
-					break;
-				case DFS_2B:
-					sys_put_be16(sys_read32(SPI_FIFO(data->base)), data->ctx.rx_buf);
-					break;
-				case DFS_1B:
-					*(uint8_t *)data->ctx.rx_buf = sys_read32(SPI_FIFO(data->base));
-					break;
+			case DFS_4B:
+				sys_put_be32(sys_read32(SPI_FIFO(data->base)), data->ctx.rx_buf);
+				break;
+			case DFS_2B:
+				sys_put_be16(sys_read32(SPI_FIFO(data->base)), data->ctx.rx_buf);
+				break;
+			case DFS_1B:
+				*(uint8_t *)data->ctx.rx_buf = sys_read32(SPI_FIFO(data->base));
+				break;
 			}
 			spi_context_update_rx(&data->ctx, data->dfs, 1);
 		}
@@ -179,15 +182,15 @@ static int spi_bcm2711_xfer(const struct device *port)
 }
 
 static int spi_bcm2711_transceive(const struct device *port, const struct spi_config *spi_cfg,
-				 const struct spi_buf_set *tx_bufs,
-				 const struct spi_buf_set *rx_bufs)
+				  const struct spi_buf_set *tx_bufs,
+				  const struct spi_buf_set *rx_bufs)
 {
 	struct spi_bcm2711_data *data = port->data;
 	int ret = 0;
 
 	spi_context_lock(&data->ctx, false, NULL, NULL, spi_cfg);
 	ret = spi_bcm2711_configure(port, spi_cfg);
-	
+
 	if (ret < 0) {
 		goto end;
 	}
@@ -223,17 +226,17 @@ static int spi_bcm2711_init(const struct device *port)
 		return -EINVAL;
 	}
 
-#define BCM2711_GPIO_BASE 0xfe200000  /* There's no API for this right now */
+#define BCM2711_GPIO_BASE  0xfe200000 /* There's no API for this right now */
 #define BCM2711_GPI0_FUNC0 0x4
 	mm_reg_t gpio_base;
 	device_map(&gpio_base, BCM2711_GPIO_BASE, 0x1000, K_MEM_CACHE_NONE);
 	uint32_t fsel = sys_read32(gpio_base + 0x00);
-	fsel |= (BCM2711_GPI0_FUNC0 << 24);  /* PIN8 */
-	fsel |= (BCM2711_GPI0_FUNC0 << 27);  /* PIN9 */
+	fsel |= (BCM2711_GPI0_FUNC0 << 24); /* PIN8 */
+	fsel |= (BCM2711_GPI0_FUNC0 << 27); /* PIN9 */
 	sys_write32(fsel, gpio_base + 0x00);
 	fsel = sys_read32(gpio_base + 0x04);
-	fsel |= (BCM2711_GPI0_FUNC0 << 0);  /* PIN10 */
-	fsel |= (BCM2711_GPI0_FUNC0 << 3);  /* PIN11 */
+	fsel |= (BCM2711_GPI0_FUNC0 << 0); /* PIN10 */
+	fsel |= (BCM2711_GPI0_FUNC0 << 3); /* PIN11 */
 	sys_write32(fsel, gpio_base + 0x04);
 #undef BCM2711_GPI0_FUNC0
 #undef BCM2711_GPIO_BASE
@@ -248,8 +251,8 @@ static int spi_bcm2711_init(const struct device *port)
 	return 0;
 }
 
-#define SPI_BCM2711_INIT(n)                                                                         \
-	static const struct spi_bcm2711_config spi_bcm2711_cfg_##n = {                               \
+#define SPI_BCM2711_INIT(n)                                                                        \
+	static const struct spi_bcm2711_config spi_bcm2711_cfg_##n = {                             \
 		DEVICE_MMIO_NAMED_ROM_INIT(reg_base, DT_DRV_INST(n)),                              \
 		.clk_gpio = GPIO_DT_SPEC_INST_GET(n, clk_gpios),                                   \
 		.miso_gpio = GPIO_DT_SPEC_INST_GET(n, miso_gpios),                                 \
@@ -257,12 +260,13 @@ static int spi_bcm2711_init(const struct device *port)
 		.cs_gpio = GPIO_DT_SPEC_INST_GET(n, cs_gpios),                                     \
 		.clock_freq = DT_PROP(DT_DRV_INST(n), clock_frequency),                            \
 	};                                                                                         \
-	static struct spi_bcm2711_data spi_bcm2711_data_##n = {                                      \
-		SPI_CONTEXT_INIT_LOCK(spi_bcm2711_data_##n, ctx),                                   \
-		SPI_CONTEXT_INIT_SYNC(spi_bcm2711_data_##n, ctx),                                   \
+	static struct spi_bcm2711_data spi_bcm2711_data_##n = {                                    \
+		SPI_CONTEXT_INIT_LOCK(spi_bcm2711_data_##n, ctx),                                  \
+		SPI_CONTEXT_INIT_SYNC(spi_bcm2711_data_##n, ctx),                                  \
 		SPI_CONTEXT_CS_GPIOS_INITIALIZE(DT_DRV_INST(n), ctx)};                             \
                                                                                                    \
-	DEVICE_DT_INST_DEFINE(n, spi_bcm2711_init, NULL, &spi_bcm2711_data_##n, &spi_bcm2711_cfg_##n, \
-			      POST_KERNEL, CONFIG_SPI_INIT_PRIORITY, &spi_bcm2711_api);
+	DEVICE_DT_INST_DEFINE(n, spi_bcm2711_init, NULL, &spi_bcm2711_data_##n,                    \
+			      &spi_bcm2711_cfg_##n, POST_KERNEL, CONFIG_SPI_INIT_PRIORITY,         \
+			      &spi_bcm2711_api);
 
 DT_INST_FOREACH_STATUS_OKAY(SPI_BCM2711_INIT)
