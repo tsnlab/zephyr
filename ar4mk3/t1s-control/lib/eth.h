26f169bfcb2bd82db53fe77874ab97acd126d72d
pak-ji <pakji@tsnlab.com>
2025-10-12 16:20:46 +0900

    Remove unnecessary arp library

--- ar4mk3/t1s-control/lib/eth.h
+++ ar4mk3/t1s-control/lib/eth.h
@@ -11,13 +11,8 @@
 
 #define ETH_ALEN 6
 #define ETH_HLEN 14
-#define ARP_HLEN 28
 
 #define ETH_P_IP  0x0800 /* Internet Protocol packet	*/
-#define ETH_P_ARP 0x0806 /* Address Resolution packet	*/
-
-#define ARPOP_REQUEST 1 /* ARP request.  */
-#define ARPOP_REPLY   2 /* ARP reply.  */
 
 #define PF_INET 2 /* IP protocol family.  */
 #define AF_INET PF_INET
@@ -32,41 +27,6 @@ struct ethhdr_l {
 	unsigned char h_dest[ETH_ALEN];
 	unsigned char h_source[ETH_ALEN];
 	unsigned short h_proto;
-};
-
-struct arphdr_l {
-	unsigned short ar_hrd;
-	unsigned short ar_pro;
-	unsigned char ar_hln;
-	unsigned char ar_pln;
-	unsigned short ar_op;
-	unsigned char ar_sha[ETH_ALEN];
-	unsigned char ar_sip[4];
-	unsigned char ar_tha[ETH_ALEN];
-	unsigned char ar_tip[4];
-};
-
-struct arphdr {
-	unsigned short int ar_hrd; /* Format of hardware address.  */
-	unsigned short int ar_pro; /* Format of protocol address.  */
-	unsigned char ar_hln;      /* Length of hardware address.  */
-	unsigned char ar_pln;      /* Length of protocol address.  */
-	unsigned short int ar_op;  /* ARP opcode (command).  */
-};
-
-struct arphdr_ipv4 {
-	struct arphdr arp;
-	uint8_t sender_mac[ETH_ALEN];
-	uint8_t sender_ip[IP_LEN];
-	uint8_t target_mac[ETH_ALEN];
-	uint8_t target_ip[IP_LEN];
-	uint32_t seq;
-};
-
-// ARP table entry regarding IPv4
-struct arp_entry {
-	uint8_t ip[IP_LEN];
-	uint8_t mac[ETH_ALEN];
 };
 
 #endif /* ETH_H */
